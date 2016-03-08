20fe2903e15628d2d7ead9eb729866c3b13b0a19
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-03-07 23:16:03 -0500

    Menu view only displays courses that have dishes in them.

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,5 +4,15 @@ class Course < ActiveRecord::Base
   def get_dishes_by_course
     Dish.where('course_id = ?', id).order(:price)
   end
+  
+  def self.get_dishes_by_course
+    results = []
+    courses = Course.all
+    courses.each do |c|
+      temp = Dish.where('course_id = ?', c.id).order(:price)
+      results << temp if !temp.empty?
+    end
+    results
+  end
 
 end
